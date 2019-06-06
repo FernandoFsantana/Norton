a5f28852f034322f74ec376c2e59e1336327bbea
GC-Aula <GC-Aula@GC-Aula-PC>
2019-06-06 20:29:26 -0300

    Fernando

--- ProgramaParimparSwing/src/com/cursoemvideo/parimpar/NewClass.java
+++ ProgramaParimparSwing/src/com/cursoemvideo/parimpar/NewClass.java
@@ -1,6 +1,6 @@
 //"Aula GC 06/06/2019
 
-//isso é chato de fazer
+//isso é chato de fazerjhzxvjzbc\
 /* * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
